c8acf15c0572a797c527e87381e30289ee9e4610
Kat Vengo <katvengo@gmail.com>
2019-07-24 21:28:44 -0700

    deploying

---
+++ client/build/precache-manifest.3e204739ee92196d8d39d1192355b141.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c657e1a48db27e5a8db",
+    "url": "/static/css/main.0fb3c018.chunk.css"
+  },
+  {
+    "revision": "7c657e1a48db27e5a8db",
+    "url": "/static/js/main.6a32ba00.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0f43c7156963d761ea84",
+    "url": "/static/js/2.81c1bcea.chunk.js"
+  },
+  {
+    "revision": "eb065565767ab5d56b82e0810ec3f34a",
+    "url": "/index.html"
+  }
+];
